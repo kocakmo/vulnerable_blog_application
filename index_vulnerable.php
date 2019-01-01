98af3fa5216f212fa8a740f36eb7b39e67b7cf27
kocakmo <46299266+kocakmo@users.noreply.github.com>
2019-01-01 22:52:42 +0300

    Add files via upload

---
+++ index_vulnerable.php
@@ -0,0 +1,98 @@
+<h1>Blog</h1>
+
+<form action='search_vulnerable.php' method='post'>
+<p><label>Search Box</label>
+    <input type='text' name='word' </p>
+	<p><input type='submit' name='search' value='Search'></p>
+</form>
+
+
+
+
+<?php		
+		
+
+			
+			
+$servername = "localhost";
+$db_username = "root";
+$db_password = "usbw";
+$dbname = "admintable";
+	// Create connection
+$conn = new mysqli($servername, $db_username, $db_password, $dbname);
+	// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+
+}
+
+$result=mysqli_query($conn,"SELECT postid,postTitle,email,content FROM comments");
+
+while($row = $result->fetch_assoc())
+{
+echo '<div>';
+                echo '<h1> POST ID: '.$row['postid'].'</h1>';
+                echo '<p>Posted by '.$row['email'].'</p>';
+                echo '<p>'.substr($row['content'],0,150).'</p>';                
+                echo '<p><a href="viewpost.php?id='.$row['postid'].'">Read More</a></p>';                
+echo '</div>';
+}
+?>
+
+<form action='' method='post'>
+
+    <p><label>Title</label><br />
+    <input type='text' name='postTitle' </p>
+
+    <p><label>Description</label><br />
+    <textarea name='postDesc' cols='60' rows='10'></textarea></p>
+
+    <p><label>Content</label><br />
+    <textarea name='postCont' cols='60' rows='10'></textarea></p>
+
+    <p><input type='submit' name='submit' value='Submit'></p>
+
+</form>
+
+<?php
+header('X-XSS-Protection:0');
+//if form has been submitted process it
+if(isset($_POST['submit'])){
+
+$servername = "localhost";
+$username = "root";
+$password = "usbw";
+$dbname = "admintable";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$title=$_POST['postTitle'];
+$description=$_POST['postDesc'];
+$content=$_POST['postCont'];
+$sql = "INSERT INTO comments (postTitle, email, content)
+VALUES ('$title', '$description', '$content')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+
+}
+
+
+?>
+
+
+
+	
+
+
---
+++ search_vulnerable.php
@@ -0,0 +1,30 @@
+<?php
+
+if(isset($_POST['search'])){
+
+$servername = "localhost";
+$db_username = "root";
+$db_password = "usbw";
+$dbname = "admintable";
+	// Create connection
+$conn = new mysqli($servername, $db_username, $db_password, $dbname);
+	// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+
+}
+$sw=$_POST['word'];
+$result=mysqli_query($conn,"SELECT * FROM comments where postTitle like '%$sw%' or content like '%$sw%'");
+
+
+while($row = $result->fetch_assoc())
+{
+echo '<div>';
+                echo '<h1> POST ID: '.$row['postid'].'</h1>';
+                echo '<p>Posted by '.$row['email'].'</p>';
+                echo '<p>'.substr($row['content'],0,150).'</p>';                
+                echo '<p><a href="viewpost.php?id='.$row['postid'].'">Read More</a></p>';                
+echo '</div>';
+}
+}
+?>
